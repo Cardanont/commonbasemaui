f1fbdc31ae85d4f9545154f7531e7111e94258d9
DanielO <11862549+Cardanont@users.noreply.github.com>
2023-10-04 03:01:39 -0600

    Last Demo changes

--- RESTDemo/MainViewModel.cs
+++ RESTDemo/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using System.Windows.Input;
 
 namespace RESTDemo
@@ -8,6 +9,7 @@ namespace RESTDemo
         HttpClient client;
         JsonSerializerOptions _serializerOptions;
         string baseUrl = "https://65193268818c4e98ac6021ad.mockapi.io";
+        private List<User> Users;
 
         public MainViewModel()
         {
@@ -19,23 +21,78 @@ namespace RESTDemo
             };
         }
 
-        public ICommand GetAllUsersCommand => 
+        public ICommand GetAllUsersCommand =>
             new Command(async () =>
         {
             var url = $"{baseUrl}/users";
-            var response = 
+            var response =
             await client.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
             {
                 //var content = await response.Content.ReadAsStringAsync();
-                using(var responseStream = await response.Content.ReadAsStreamAsync())
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
                 {
-                    var data = 
+                    var data =
                     await JsonSerializer.DeserializeAsync<List<User>>(responseStream, _serializerOptions);
+                    Users = data;
                 }
             }
         });
 
+        public ICommand GetSingleUserCommand =>
+            new Command(async () =>
+            {
+                var url = $"{baseUrl}/users/23";
+                var response =
+                    await client.GetStringAsync(url);
+
+            });
+
+
+        public ICommand AddUserCommand =>
+            new Command(async () =>
+            {
+                var url = $"{baseUrl}/users";
+
+                // add a new user
+                var user =
+                new User
+                {
+                    createdAt = DateTime.Now,
+                    name = "Cardan",
+                    avatar = "https://avatars2.githubusercontent.com/u/2314951?s=460&v=4"
+                };
+
+                var json = JsonSerializer.Serialize(user, _serializerOptions);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response =
+                    await client.PostAsync(url, content);
+            });
+
+        public ICommand UpdateUserCommand => new Command(async () =>
+        {
+            var user = Users.FirstOrDefault(x => x.id == "1");
+            var url = $"{baseUrl}/users/1";
+
+            user.name = "Dante";
+
+            var json = JsonSerializer.Serialize<User>(user, _serializerOptions);
+
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response =
+                await client.PutAsync(url, content);
+        });
+
+        public ICommand DeleteUserCommand => new Command(async () =>
+        {
+            var url = $"{baseUrl}/users/51";
+
+            var response =
+                await client.DeleteAsync(url);
+        });
     }
 }
