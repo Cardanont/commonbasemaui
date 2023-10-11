434967dec5fe3bb7931381ab5f8f76042b499f76
DanielO <11862549+Cardanont@users.noreply.github.com>
2023-10-11 17:34:38 -0600

    Database_Access_Class

---
+++ SQLiteDemo/MVVM/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace SQLiteDemo.MVVM.Models
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [Column("name"), Indexed, NotNull]
+        public string Name { get; set; }
+        [Unique]
+        public string Phone { get; set; }
+        public int Age { get; set; }
+        [MaxLength(100)]
+        public string Address { get; set; }
+        [Ignore]
+        public bool IsYoung => Age > 50 ? true : false;
+    }
+}
