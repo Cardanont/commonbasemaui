4ad910ced37449d9c756b3b1835a593aa07eccb2
DanielO <11862549+Cardanont@users.noreply.github.com>
2023-10-27 22:21:57 -0600

    One to Many

--- SQLiteDemo/MVVM/Models/Passport.cs
+++ SQLiteDemo/MVVM/Models/Passport.cs
@@ -1,9 +1,13 @@
 ﻿using SQLiteDemo.Abstractions;
+using ForeignKeyAttribute = SQLiteNetExtensions.Attributes.ForeignKeyAttribute;
 
 namespace SQLiteDemo.MVVM.Models
 {
     public class Passport : TableData
     {
         public DateTime ExpirationDate { get; set; }
+
+        [ForeignKey(typeof(Customer))]
+        public int CustomerId { get; set; }
     }
 }
