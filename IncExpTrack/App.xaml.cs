661f9fcdd35d0a63a4dce474ee2cd43b7d7df093
DanielO <11862549+Cardanont@users.noreply.github.com>
2023-10-30 23:33:41 -0600

    Income and Expenses Tracker FC

---
+++ IncExpTrack/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IncExpTrack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
