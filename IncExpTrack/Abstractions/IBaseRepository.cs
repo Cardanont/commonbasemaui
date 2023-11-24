85dbdad8a91b21d3e8c429b37dc4f9a211df22a5
DanielO <11862549+Cardanont@users.noreply.github.com>
2023-11-23 23:06:58 -0600

    Creating the Model

---
+++ IncExpTrack/Abstractions/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncExpTrack.Abstractions
+{
+    public interface IBaseRepository<T> : IDisposable
+        where T : TableData, new()
+    {
+        void SaveItem(T item);
+        void SaveItemWIthChildren(T item, bool recursive = false);
+        T GetItem(int Id);
+        T GetItem(Expression<Func<T, bool>> predicate);
+        List<T> GetItems();
+        List<T> GetItems(Expression<Func<T, bool>> predicate);
+        List<T> GetItemsWithChildren();
+        void DeleteItem(T item);
+    }
+}
---
+++ IncExpTrack/Constants.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+
+namespace IncExpTrack
+{
+    public static class Constants
+    {
+        private const string DBFileName = "SQLite.db001";
+
+        public const SQLiteOpenFlags Flags =
+            // open the database in read/write mode
+            SQLiteOpenFlags.ReadWrite |
+            // create the database if it doesn't exist
+            SQLiteOpenFlags.Create |
+            // enable multi-threaded database access
+            SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                return Path.Combine(FileSystem.AppDataDirectory, DBFileName);
+            }
+        }
+    }
+}
